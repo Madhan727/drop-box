fbed187f736ceb64cb65ff064435bd713fa64376
Madhan <madhand727@gmail.com>
2026-02-04 21:18:40 +0530

    Update ALLOWED_HOSTS to allow all hosts

--- localdrop/settings.py
+++ localdrop/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
