954f0aeb5a8c19206c1fa48c6b5331f608352188
Madhan <madhand727@gmail.com>
2026-02-04 20:59:23 +0530

    Remove default secret key

--- localdrop/settings.py
+++ localdrop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG") == "True"
 
